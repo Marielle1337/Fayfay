10e07c44ee60c478f1622621fe839a1333585074
Marielle Antoine <marielle010495@gmail.com>
2020-11-04 19:11:35 +0100

    Set assets

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,13 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('main', './assets/main.js')
+    //.addEntry('bootstrap', './assets/boostrap.min.js')
+    .addEntry('owl-carousel', './assets/owl.carousel.min.js')
+    .addEntry('tooltip', './assets/typed.js')
+    .addEntry('typed', './assets/typed.js')
+    .addEntry('typewriter', './assets/typewriter.js')
+    .addEntry('wow', './assets/wow.min.js')
     //.addEntry('page1', './assets/page1.js')
     //.addEntry('page2', './assets/page2.js')
 
